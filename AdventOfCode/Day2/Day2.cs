38ba160126339bd01142ed3fc212c3ece0a0c942
Michael Knowles <michaelknowles.rhs@gmail.com>
2019-02-09 14:42:36 -0800

    Started Day 2; got input file.

---
+++ AdventOfCode/Day2/Day2.cs
@@ -0,0 +1,9 @@
+namespace AdventOfCode
+{
+    public class Day2
+    {
+        private List<string> BoxIDs;
+
+        public Day2(List<string> boxIDs) => BoxIDs = boxIDs;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,12 +9,11 @@ namespace AdventOfCode
         {
             // Day 1
             Console.WriteLine("Day 1");
-            string[] changeStrings = System.IO.File.ReadAllLines("Day1-Input.txt");
+            var changeStrings = readInput("Day1-Input.txt");
             List<int> changes = new List<int>();
-            for(int i = 0; i < changeStrings.Length; i++)
+            foreach (var t in changeStrings)
             {
-                int change;
-                Int32.TryParse(changeStrings[i], out change);
+                Int32.TryParse(t, out var change);
                 changes.Add(change);
             }
             Day1 day1 = new Day1(changes);
@@ -22,6 +21,15 @@ namespace AdventOfCode
             Console.WriteLine(day1.GetFrequency());
             Console.WriteLine("Part 2:");
             Console.WriteLine(day1.GetSameFrequency());
+            
+            // Day 2
+            Console.WriteLine("Day 2");
+            var boxIDStrings = readInput("Day2-Input.txt");
+        }
+
+        static IEnumerable<string> readInput(string fileName)
+        {
+            return System.IO.File.ReadAllLines(fileName);
         }
     }
 }
